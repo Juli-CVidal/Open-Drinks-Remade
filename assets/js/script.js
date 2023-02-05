4b472e3b11653749deb9944176e2a7d6a8cfaaea
Juli-CVidal <juli.vidal.019@gmail.com>
2023-02-05 20:48:31 -0300

    Added Home Slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,23 @@
-
-
 /*=============== SWIPER PROJECTS ===============*/
 let swiperProjects = new Swiper(".home__container", {
-    loop: true,
-    spaceBetween: 24,
-    slidesPerView: 1,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-      el: ".swiper-pagination",
-    },
-    mousewheel: true,
-    keyboard: true,
-  });
+  spaceBetween: 10,
+
+  effect: 'coverflow',
+  coverflowEffect: {
+    rotate: 30,
+    slideShadows: false,
+  },
+  loop: true,
+  autoplay: {
+    delay: 5000,
+  },
+  slidesPerView: 1,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  keyboard: true,
+});
