a06b03a7ac87759a0ee701f95d0de69ff4ec31bf
Juli-CVidal <juli.vidal.019@gmail.com>
2023-02-07 11:46:47 -0300

    Starting with api - Incomplete

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ const navMenu = document.getElementById("nav-menu"),
 if (navToggle) {
   navToggle.addEventListener("click", () => {
     navMenu.classList.add("show-menu");
-    navToggle.style.opacity=0;
+    navToggle.style.opacity = 0;
   });
 }
 
@@ -29,14 +29,12 @@ const linkAction = () => {
 };
 navLink.forEach((n) => n.addEventListener("click", linkAction));
 
-
-
 /*=============== SWIPER SLIDES ===============*/
 let swiperProjects = new Swiper(".home__container", {
   spaceBetween: 10,
-  
-  effect: 'fade',
-  speed:750,
+
+  effect: "fade",
+  speed: 750,
   coverflowEffect: {
     rotate: 30,
     slideShadows: false,
@@ -49,15 +47,87 @@ let swiperProjects = new Swiper(".home__container", {
   slidesPerView: 1,
 });
 
+/*===============  CATALOGUE ===============*/
+class Drink {
+  constructor(id, name, category, image) {
+    this.id = id;
+    this.name = name;
+    this.category = category;
+    this.image = image;
+  }
+}
+const API_URL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?";
+const DRINKS_CONTAINER = document.getElementById("drinks");
+let drinks = [];
 
+const createDrink = (drink) => {
+  const container = document.createElement("div");
+  const div = document.createElement("div");
+  div.id = drink.id;
+  div.classList.add(`media`);
+  div.classList.add(drink.category);
+  const divLeft = document.createElement("div");
+  divLeft.classList.add("media-left");
+  const img = document.createElement("img");
+  img.src = drink.image;
+  img.alt = "";
+  divLeft.appendChild(img);
 
-/*=============== SWIPER CATALOGUE ===============*/
-const options = document.querySelectorAll('.option');
-options.forEach(option => {
-  option.addEventListener('click', function() {
-    options.forEach(option => {
-      option.classList.remove('selected');
+  const divBody = document.createElement("div");
+  divBody.classList.add("media-body");
+  const h4 = document.createElement("h4");
+  h4.classList.add("media-heading");
+  h4.textContent = drink.name;
+  divBody.appendChild(h4);
+
+  div.appendChild(divLeft);
+  div.appendChild(divBody);
+  container.appendChild(div);
+  return container;
+};
+
+const CATEGORIES = [
+  "Beer",
+  "Cocktail",
+  // "Shot",
+  // "Coffe / Tea",
+  // "Homenade Liqueur",
+  // "Punch / Party Drink",
+  // "Soft Drink",
+  // ,"Non_Alcoholic"
+];
+
+async function getCategory(category) {
+  let drinkElement;
+  const response = await fetch(API_URL + "c=" + category);
+  const responseJson = await response.json();
+  responseJson.drinks.forEach((drink) => {
+    drinkElement = new Drink(
+      drink.idDrink,
+      drink.strDrink,
+      category,
+      drink.strDrinkThumb
+    );
+    drinks.push(drinkElement);
+  });
+}
+
+function getAllDrinks() {
+  CATEGORIES.forEach((category) => {
+    getCategory(category);
+  });
+  drinks.sort((a, b) => {
+    return a.name - b.name;
+  });
+}
+getAllDrinks();
+
+const options = document.querySelectorAll(".option");
+options.forEach((option) => {
+  option.addEventListener("click", function () {
+    options.forEach((option) => {
+      option.classList.remove("selected");
     });
-    this.classList.add('selected');
+    this.classList.add("selected");
   });
 });
